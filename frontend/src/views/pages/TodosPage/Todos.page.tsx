e61781ce3c73e54114c9587a957a59313ed70ec3
kenji132 <nishiken.dev@gmail.com>
2023-09-23 18:29:34 +0900

    add todo form

--- frontend/src/views/pages/TodosPage/Todos.page.tsx
+++ frontend/src/views/pages/TodosPage/Todos.page.tsx
@@ -1,27 +1,108 @@
-import { Button } from "@chakra-ui/react"
+import { AppEnv } from "@/constants/env";
+import { Todo } from "@/modules/todo/type";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+type FormData = {
+  title: string;
+  content: string;
+};
 
 export const Todos = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [formData, setFormData] = useState<FormData>({
+    title: "",
+    content: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(JSON.stringify(formData));
+    await fetch(`${AppEnv.apiUrl}/todos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    setFormData({ title: "", content: "" });
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <>
-      <h1>Todo index</h1>
-      <Button colorScheme="teal" onClick={() => {
-        fetch("http://localhost:1323/todos")
-          .then(res => {
-            if (!res.ok) {
-              throw new Error('APIレスポンスがエラーを返しました。');
-            }
-            return res.json();
-          })
-          .then(data => {
-            console.log(data);
-          })
-          .catch(error => {
-            console.error('APIリクエストエラー:', error);
-          });
-      }}>
-        Get Todos
-      </Button>
-    </>
-  )
-}
+      <VStack>
+        <Button
+          colorScheme="teal"
+          onClick={() => {
+            fetch(`${AppEnv.apiUrl}/todos`)
+              .then((res) => {
+                if (!res.ok) {
+                  throw new Error("APIレスポンスがエラーを返しました。");
+                }
+                return res.json();
+              })
+              .then((data) => {
+                setTodos(data);
+              })
+              .catch((error) => {
+                console.error("APIリクエストエラー:", error);
+              });
+          }}
+        >
+          Get Todos
+        </Button>
 
+        <Text fontSize={"3xl"}>Index</Text>
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <Text fontSize={"xl"}>Title: {todo.title}</Text>
+              <Text fontSize={"xl"}>Content: {todo.content}</Text>
+            </div>
+          );
+        })}
+        <form onSubmit={handleSubmit}>
+          <FormControl mt={3}>
+            <FormLabel>Title</FormLabel>
+            <Input
+              type="text"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl mt={3}>
+            <FormLabel>Content</FormLabel>
+            <Input
+              type="text"
+              id="content"
+              name="content"
+              value={formData.content}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <Button type="submit" mt={3} justifyContent={"center"}>
+            Create Todo
+          </Button>
+        </form>
+      </VStack>
+    </>
+  );
+};
